e3fca1672b27f058bef3c76976a8f89560b1cdf4
Sohan Samanta <sohansamanta18@gmail.com>
2025-10-24 14:42:27 +0530

    Update App.js

---
+++ app.js
@@ -0,0 +1,11 @@
+let gameSeq = [];
+let userSeq = [];
+
+let started = false;
+let level = 0;
+
+//Start Game.
+document.addEventListener("keypress", function() {
+    console.console.log("Game Started");
+    
+});
