71df189e134e6ad351eb207c1cab9b575083b27e
Sohan Samanta <sohansamanta18@gmail.com>
2025-10-24 20:02:19 +0530

    Updating files.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let gameSeq = [];
 let userSeq = [];
 
-let btn = ["red", "yellow", "green", "purple"];
+let btns = ["red", "yellow", "green", "purple"];
 let started = false;
 let level = 0;
 
@@ -24,17 +24,37 @@ function levelUp() {
 
     //select random btn.
     let randomIndex = Math.floor(Math.random()*3);
-    let randomColor = btn[randomIndex];
+    let randomColor = btns[randomIndex];
     let randomBtn = document.querySelector(`.${randomColor}`);
-    btnflash(randomBtn);
+    gameFlash(randomBtn);
     console.log(randomIndex);
     console.log(randomColor);
     console.log(randomBtn);
 }
 
-function btnflash(btn){
+function gameFlash(btn){
     btn.classList.add("flash");
     setTimeout(function(){
         btn.classList.remove("flash");
     }, 280);
 }
+
+function userFlash(btn){
+    btn.classList.add("userFlash");
+    setTimeout(function(){
+        btn.classList.remove("userFlash");
+    }, 280);
+}
+
+//btn press(Event Listner).
+function btnPress() {
+    console.log("Button was pressed");
+    let btn = this;
+    userFlash(btn);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click", btnPress);
+}
+
