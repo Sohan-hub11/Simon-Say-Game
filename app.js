12d5926899a19ad84b5d7a34a547055dec180199
Sohan Samanta <sohansamanta18@gmail.com>
2025-10-24 15:59:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let gameSeq = [];
 let userSeq = [];
 
+let btn = ["red", "yellow", "green", "purple"];
 let started = false;
 let level = 0;
 
@@ -21,4 +22,19 @@ function levelUp() {
     level++;
     h2.innerText = `Level ${level}`;
 
+    //select random btn.
+    let randomIndex = Math.floor(Math.random()*3);
+    let randomColor = btn[randomIndex];
+    let randomBtn = document.querySelector(`.${randomColor}`);
+    btnflash(randomBtn);
+    console.log(randomIndex);
+    console.log(randomColor);
+    console.log(randomBtn);
+}
+
+function btnflash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    }, 280);
 }
