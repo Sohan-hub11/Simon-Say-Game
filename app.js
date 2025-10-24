fabe2a3bddc5acea0e921ec4a6acb36e71d6b4c5
Sohan Samanta <sohansamanta18@gmail.com>
2025-10-24 14:54:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,21 @@ let userSeq = [];
 let started = false;
 let level = 0;
 
+let h2 = document.querySelector("h2");
+
 //Start Game.
 document.addEventListener("keypress", function () {
     if(started == false){
         console.log("Game Started");
         started = true;
+
+        levelUp();
     }
 });
+
+//btn-flashing and updating the Level.
+function levelUp() {
+    level++;
+    h2.innerText = `Level ${level}`;
+
+}
